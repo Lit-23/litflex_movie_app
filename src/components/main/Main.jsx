dbf22fffd83b542d304bc1f7a2f2be833dcd94bb
Lit-23 <rommelmarquez143@gmail.com>
2023-12-09 06:11:06 -0800

    added authentication state observer and conditional rendering

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,11 +3,31 @@ import Popular from "./popular/Popular";
 import TopRated from "./topRated/TopRated";
 import Upcoming from "./upcoming/Upcoming";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import firebaseApp from '../authentication/firebaseConfig';
 
 export default function Main() {
   const [authenticated, setAuthenticated] = useState(false);
+  // const [userProperties, setUserProperties] = useState({});
+  // const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const auth = getAuth(firebaseApp);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        console.log(user)
+        // ...
+        setAuthenticated(true);
+        // setUserProperties(user);
+        // setUserName(user.displayName);
+      }
+    });
+  }, [])
 
   return (
     <main>
@@ -15,6 +35,7 @@ export default function Main() {
         !authenticated 
           ? <About /> 
           : <>
+              <p></p>
               <Popular />
               <TopRated />
               <Upcoming />
